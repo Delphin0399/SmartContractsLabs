d4a0f65cb91743af5cbf419de086d51dd3165cb9
delphin0399@rambler.ru <delphin0399@rambler.ru>
2022-11-15 02:03:21 +0300

    revised config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+require("@nomicfoundation/hardhat-toolbox");
 
-export default config;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const API_URL = process.env.API_URL;
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
+};
